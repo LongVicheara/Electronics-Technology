2ea24226b29cdea2d5b511cbaa4011177dd9658a
VICHEARA07 <146500298+LongVicheara@users.noreply.github.com>
2024-02-10 21:38:08 +0700

    updata

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <router-view />
+  <h1 class="title">Hi</h1>
+  {{ newTitle }}
 </template>
 
-<script setup>
+<style>
+.title{
+  color: rgb(183, 0, 0);
+}
+</style>
 
+<script setup lang="ts">
+
+const newTitle = "World"
 </script>

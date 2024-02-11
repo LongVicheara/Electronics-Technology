fa0b7ce7778064269d3de8c3d42aeb4cdecd4c06
VICHEARA07 <146500298+LongVicheara@users.noreply.github.com>
2024-02-11 11:30:03 +0700

    updata_product

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-
+import Product from '../views/Product.vue';
 
 const routes = [
   {
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,16 +1,13 @@
 <template>
     <div>
         <Navbar />
+
     </div>
     <div>
-        <img src="../assets/img/banner-1.png" alt="banner-1">
+        <h1>HI</h1>
     </div>
-
 </template>
 
 <script setup>
 import Navbar from '../components/Navbar.vue';
-
-
 </script>
-
